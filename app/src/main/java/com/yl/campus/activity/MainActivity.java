ff2d89c2b3e30c128178bc081970e7392d7d430a
Yangli <yangl7240@gmail.com>
2017-09-27 17:33:14 +0800

    利用banner实现图片轮播

--- app/src/main/java/com/yl/campus/activity/MainActivity.java
+++ app/src/main/java/com/yl/campus/activity/MainActivity.java
@@ -1,7 +1,10 @@
 package com.yl.campus.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.yl.campus.R;
 
@@ -11,5 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button news = (Button) findViewById(R.id.news_button);
+        news.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, NewsActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/yl/campus/activity/NewsActivity.java
+++ app/src/main/java/com/yl/campus/activity/NewsActivity.java
@@ -1,15 +1,43 @@
 package com.yl.campus.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.yl.campus.R;
+import com.yl.campus.model.NewsModel;
+import com.yl.campus.model.OnGetImageListener;
+import com.youth.banner.Banner;
+
+import java.util.List;
 
 public class NewsActivity extends AppCompatActivity {
+
+    private Banner banner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news);
+        banner = (Banner) findViewById(R.id.banner);
+        banner.setImageLoader(new GlideImageLoader());
+        NewsModel model = new NewsModel();
+        model.getTopImage(new OnGetImageListener() {
+            @Override
+            public void onSucceed(final List<String> images) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        banner.setImages(images);
+                        banner.start();
+                    }
+                });
+            }
+
+            @Override
+            public void onFailed() {
+
+            }
+        });
+
     }
 }
