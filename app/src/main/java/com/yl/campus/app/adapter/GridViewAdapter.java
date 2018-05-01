6ba246a1a8514c503678caa613c117b12de113a7
Yangli <yangl7240@gmail.com>
2018-05-01 17:36:28 +0800

    更新图标资源

--- app/src/main/java/com/yl/campus/app/adapter/GridViewAdapter.java
+++ app/src/main/java/com/yl/campus/app/adapter/GridViewAdapter.java
@@ -12,17 +12,23 @@ import android.widget.TextView;
 import com.yl.campus.R;
 import com.yl.campus.app.activity.MainActivity;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 /**
  * 功能网格适配器
  * Created by Luke on 2017/9/28.
  */
 
 public class GridViewAdapter extends BaseAdapter {
-
+    @BindView(R.id.itemImage)
+    ImageView ivItem;
+    @BindView(R.id.itemName)
+    TextView tvItem;
     private final String[] functionNames = {"新闻展示", "我的课表", "图书搜索",
             "宿舍生活", "系统设置"};
-    private final int[] functionImages = {R.drawable.ic_home, R.drawable.ic_home,
-            R.drawable.ic_home, R.drawable.ic_home, R.drawable.ic_home};
+    private final int[] functionImages = {R.drawable.ic_news, R.drawable.ic_timetable,
+            R.drawable.ic_books, R.drawable.ic_dormitory, R.drawable.ic_setting};
 
     @Override
     public int getCount() {
@@ -50,8 +56,9 @@ public class GridViewAdapter extends BaseAdapter {
         int height = (displayMetrics.heightPixels - 400) / 3; // TODO: 2017/10/9 优化
         view.setLayoutParams(new GridView.LayoutParams(
                 GridView.LayoutParams.MATCH_PARENT, height));
-        ImageView ivItem = (ImageView) view.findViewById(R.id.itemImage);
-        TextView tvItem = (TextView) view.findViewById(R.id.itemName);
+//        ImageView ivItem = (ImageView) view.findViewById(R.id.itemImage);
+//        TextView tvItem = (TextView) view.findViewById(R.id.itemName);
+        ButterKnife.bind(this, view);
         tvItem.setText(functionNames[position]);
         ivItem.setImageResource(functionImages[position]);
         return view;
